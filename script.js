d009e4ee2c3f4e5d3400cd72ea7a2f5b46e8dc68
--replace-all <r.carolinjeba@outlook.com>
2022-03-01 10:19:05 +0530

    Expense tracker project files

---
+++ script.js
@@ -0,0 +1,117 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 },
+// ];
+
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
+
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+// add transaction list to DOM
+function addTransactiontoDOM(transaction) {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `${transaction.text} 
+  <span>${sign}${Math.abs(transaction.amount)}</span>
+  <button class="delete-btn" onClick = removeTransaction(${
+    transaction.id
+  })>x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+function init() {
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactiontoDOM);
+
+  updateValues();
+}
+
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
+
+// Add new transaction object
+function addTransaction(e) {
+  e.preventDefault();
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please enter all fields');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
+
+    transactions.push(transaction);
+
+    addTransactiontoDOM(transaction);
+
+    updateValues();
+
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 10000000);
+}
+
+// removeTransaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  updateLocalStorage();
+
+  init();
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+// Init app
+init();
+
+form.addEventListener('submit', addTransaction);
